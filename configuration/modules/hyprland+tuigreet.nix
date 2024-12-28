caa639343a0d6a113c3a78983d227edcb53883cb
Craft <mariocaden@gmail.com>
2024-12-27 22:58:59 -0600

    Why it no work on login

--- configuration/modules/hyprland+tuigreet.nix
+++ configuration/modules/hyprland+tuigreet.nix
@@ -1,26 +1,26 @@
+{ inputs, pkgs, host, ... }:
 
-{ inputs, config, pkgs, ... }:
-
-{
+let 
+    hyprland-package = if host == "uconsole" then
+            inputs.uconsole-hyprland.packages.${pkgs.system}.hyprland
+        else 
+            inputs.hyprland.packages.${pkgs.system}.hyprland;
+in {
     programs.hyprland = {
         enable = true;
-        package = inputs.uconsole-hyprland.packages.${pkgs.system}.hyprland;
-        # portalPackage = inputs.uconsole-hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-        # package = inputs.uconsole-hyprland.packages.${pkgs.system}.hyprland.override {
-        #     legacyRenderer = true;
-        #     mesa = pkgs.mesa;
-        # };
-        # xwayland.enable = true;
+        package = hyprland-package; 
+        xwayland.enable = true;
     };
+
     services.greetd = {
         enable = true;
         settings = {
             initial_session = {
-                command = "${inputs.uconsole-hyprland.packages.${pkgs.system}.hyprland}/bin/Hyprland";
+                command = "${hyprland-package}/bin/Hyprland";
                 user = "user";
             };
             default_session = {
-                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --greeting 'Welcome to NixOS' --asterisks --remember --remember-user-session --time --cmd ${inputs.uconsole-hyprland.packages.${pkgs.system}.hyprland}/bin/Hyprland";
+                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --greeting 'Welcome to NixOS' --asterisks --remember --remember-user-session --time --cmd ${hyprland-package}/bin/Hyprland";
                 user = "greeter";
             };
         };
--- modules/nvim/lua/options.lua
+++ modules/nvim/lua/options.lua
@@ -8,3 +8,4 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.wo.wrap = false
 vim.o.splitright = true
+vim.g.mapleader = ' '
