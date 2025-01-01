ba2dffacbe1db85ace7fe612d56c8436bfe460b6
Craft <mariocaden@gmail.com>
2024-12-31 20:44:51 -0600

    I want zen on uconsole

--- configuration/hosts/uconsole/default.nix
+++ configuration/hosts/uconsole/default.nix
@@ -25,7 +25,7 @@
     environment.systemPackages = with pkgs; [
         home-manager
         tmux
-    ];
+    ] ++ [ inputs.zen-browser.packages.x86_64-linux.specific ];
 
     services.openssh = {
         enable = true;
