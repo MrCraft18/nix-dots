746f04e1c52efbeeb2f482c02c3fe86cbd94ca6b
Craft <mariocaden@gmail.com>
2024-12-31 20:02:06 -0600

    uncomment hyprland input and remove the desktop portal thing in hyprland.portalPakcgae

--- configuration/modules/hyprland+tuigreet.nix
+++ configuration/modules/hyprland+tuigreet.nix
@@ -6,7 +6,7 @@ in {
     programs.hyprland = {
         enable = true;
         package = inputs.${hyprland}.packages.${pkgs.system}.hyprland; 
-        portalPackage = inputs.${hyprland}.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+        # portalPackage = inputs.${hyprland}.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
         xwayland.enable = true;
     };
 
