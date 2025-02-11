aada290b9ab39e0b40331f52fd75ba0c90b282aa
MrCraft18 <mariocaden@gmail.com>
2025-02-10 20:37:32 -0600

    ok

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -13,6 +13,10 @@
     # Enable Sound
     services.pipewire = {
         enable = true;
+        wireplumber.enable = true;
+        audio.enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
         pulse.enable = true;
     };
 
--- modules/nvim/lua/nvim-lspconfig.lua
+++ modules/nvim/lua/nvim-lspconfig.lua
@@ -3,3 +3,6 @@ local lspconfig = require("lspconfig")
 
 lspconfig.lua_ls.setup({})
 lspconfig.nil_ls.setup({})
+lspconfig.cssls.setup({
+    capabilities = require('cmp_nvim_lsp').default_capabilities()
+})
