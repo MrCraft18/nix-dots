3d6ea2b105add0cbb6e0a331f2cfdc9179d2f65c
MrCraft18 <mariocaden@gmail.com>
2025-03-03 06:38:05 -0600

    old laptop firewall

--- hosts/old-laptop/default.nix
+++ hosts/old-laptop/default.nix
@@ -20,6 +20,8 @@
         };
     };
 
+    networking.firewall.allowedTCPPorts = [ 6501 ];
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
