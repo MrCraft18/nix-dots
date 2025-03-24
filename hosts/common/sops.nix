45819f8b3ab619291c6423c71dbce87c0b0975c3
MrCraft18 <mariocaden@gmail.com>
2025-03-24 23:46:58 +0900

    this is annoying

--- hosts/common/sops.nix
+++ hosts/common/sops.nix
@@ -10,14 +10,14 @@
         age = {
             # The path to the private ssh host key for this host so it can be used to decrypt the secrets encrypted with this host's public ssh host key.
             # sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519" ];
-            sshKeyPaths = [ "/home/craft/.ssh/id_ed25519" ];
+            # sshKeyPaths = [ "/home/craft/.ssh/id_ed25519" ];
             
             # This is where the age private key will be after its generate from the ssh host private key.
             # keyFile = "/var/lib/sops-nix/key.txt";
             keyFile = "/home/craft/.config/sops/age/keys.txt";
 
             # Generate the private age private key from the ssh host private key if its not there.
-            generateKey = true;
+            # generateKey = true;
         };
 
         secrets = {
