91c5461b8d56020cb85bcee8eea654d00824be78
MrCraft18 <mariocaden@gmail.com>
2025-05-07 16:03:54 +0900

    feat: add aarch64 to netbook emulated systems

--- hosts/netbook/default.nix
+++ hosts/netbook/default.nix
@@ -18,6 +18,10 @@
         };
     };
 
+    
+    boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+    nix.settings.extra-platforms = config.boot.binfmt.emulatedSystems;
+
     services.openssh = {
         enable = true;
         settings = {
