41c9338045c322e8683132d51758117839f97bf7
MrCraft18 <mariocaden@gmail.com>
2025-08-04 15:09:21 -0400

    fix: update background image generation for stylix

--- modules/stylix/stylix.nix
+++ modules/stylix/stylix.nix
@@ -6,9 +6,6 @@
 
         base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark-dark.yaml";
         
-        image = pkgs.runCommand "image.png" { } ''
-            COLOR=$(${pkgs.yq}/bin/yq -r .palette.base00 ${config.stylix.base16Scheme})
-            ${pkgs.imagemagick}/bin/magick -size 1920x1080 xc:$COLOR $out
-        '';
+        image = config.lib.stylix.pixel "base00";
     };
 }
