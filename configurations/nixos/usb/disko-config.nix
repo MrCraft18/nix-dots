8a60bc01ea71522933f0aff54b0cd64f547c5e62
MrCraft18 <mariocaden@gmail.com>
2025-09-05 18:51:14 -0500

    wip

--- configurations/nixos/usb/disko-config.nix
+++ configurations/nixos/usb/disko-config.nix
@@ -2,7 +2,7 @@
     disko.devices = {
         disk = {
             main = {
-                device = "/dev/disk/by-id/usb-PNY_USB_2.0_FD_AAB81A01000000B8-0:0";
+                device = "/dev/disk/by-id/usb-USB_SanDisk_3.2Gen1_04012dca57ff294a5e0dc9027004d4149e3657ad9c55907872b31407068274cc5b6600000000000000000000625084c3ff0910188155810766b3209e-0:0";
                 type = "disk";
                 content = {
                     type = "gpt";
