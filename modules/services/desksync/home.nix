b059c02d8255386221cfea4d2ad807e874480359
MrCraft18 <mariocaden@gmail.com>
2025-10-05 19:59:30 -0500

    feat: intialize desksync module and add basic config for lan-mouse

---
+++ modules/services/desksync/home.nix
@@ -0,0 +1,15 @@
+{ configurationName, config, lib, pkgs, ... }:
+
+let
+    cfg = config.moduleLoadout.services.desksync;
+in {
+    options.moduleLoadout.services.desksync = {
+        enable = lib.mkEnableOption "desksync service module";
+    };
+
+    config = lib.mkIf cfg.enable {
+        programs.lan-mouse = {
+            enable = true;
+        };
+    };
+}
