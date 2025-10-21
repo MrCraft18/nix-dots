7cc6c354685e1ce4a7967d97d65358ba16c29dd1
MrCraft18 <mariocaden@gmail.com>
2025-10-20 21:27:42 -0500

    fix: remove home ssh config since its no longer needed

--- modules/services/ssh/home.nix
+++ modules/services/ssh/home.nix
@@ -8,26 +8,6 @@ in {
     };
 
     config = lib.mkIf cfg.enable {
-        sops.templates = {
-            "ssh-config".content = ''
-                Host old-laptop
-                    Port 3500
-
-                Host chromebook-a
-                    Port 3500
-
-                Host chromebook-b
-                    Port 3500
-            '';
-        };
-
-        programs.ssh = {
-            enable = true;
-            includes = [
-                config.sops.templates."ssh-config".path
-            ];
-        };
-
         sops.secrets."ssh/client".path = "${config.home.homeDirectory}/.ssh/id_ed25519";
     };
 }
