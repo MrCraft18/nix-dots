d3dfb2f748b4cfd04423a3599e13ce2dafd8ab2c
MrCraft18 <mariocaden@gmail.com>
2025-10-22 00:08:01 -0500

    feat: update proton run command for yazi

--- modules/terminal/file-browsers/yazi/home.nix
+++ modules/terminal/file-browsers/yazi/home.nix
@@ -17,12 +17,11 @@ in {
 
         xdg.configFile."yazi/yazi.toml".source = pkgs.writeText "yazi.toml" ''
             [opener]
-            wine = [ { run = 'wine "$@"', orphan = true, for = "unix", desc = "Run with Wine" } ]
-            proton = [ { run = 'GAMEID=0 PROTON_VERB=run WINEPREFIX=~/.proton umu-run "$@" > /home/craft/thelog.txt 2>&1', orphan = true, for = "unix", desc = "Run with Proton" } ]
+            proton = [ { run = 'WINEPREFIX=~/.umu-prefix PROTON_VERB=run umu-run "$@"', orphan = true, for = "unix", desc = "Run with Proton" } ]
 
             [open]
             prepend_rules = [
-                { mime = "application/microsoft.portable-executable", use = [ "wine", "proton" ] }
+                { mime = "application/microsoft.portable-executable", use = [ "proton" ] }
             ]
         '';
     };
