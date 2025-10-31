07af3a5ae99936e0d21d080fd50dcd26f2aedd2a
MrCraft18 <mariocaden@gmail.com>
2025-10-30 23:56:22 -0400

    fix: add zflip ssh key back to authorizedKeys

--- modules/essentials/user.nix
+++ modules/essentials/user.nix
@@ -13,11 +13,8 @@
         hashedPasswordFile = config.sops.secrets."craft_password".path;
 
         openssh.authorizedKeys.keyFiles = map (name: ../../configurations/nixos + "/${name}/ssh_client.pub") (builtins.attrNames (builtins.readDir ../../configurations/nixos));
-        # openssh.authorizedKeys.keys = [
-        #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlvRA8dmnopz4KqdRhC4fPGkBGKA+SnTbw9ubFSEVD4 craft@desktop"
-        #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIlIQ73VqgtCDpdlaUcskdpRNteq6Bb6D8YnDF/enp7K craft@netbook"
-        #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWz8lyOKlKD3XfiYq8ZPwpLszoWRXYltCBVvQB1JQT/ nix-on-droid@localhost"
-        #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIS6/tzfk88+XtidEH8GLbDX3uf1OZ6lCL6MfDzJO5zP craft@panasonic"
-        # ];
+        openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWz8lyOKlKD3XfiYq8ZPwpLszoWRXYltCBVvQB1JQT/ nix-on-droid@localhost"
+        ];
     };
 }
