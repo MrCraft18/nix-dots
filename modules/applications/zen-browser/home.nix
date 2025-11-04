b530bcdf18e579af0edfb3104afe59060d8c2cab
MrCraft18 <mariocaden@gmail.com>
2025-11-03 20:45:20 -0600

    fix: make stylix option in zen-browser home.nix be conditional

--- modules/applications/zen-browser/home.nix
+++ modules/applications/zen-browser/home.nix
@@ -1,31 +1,21 @@
-{ configurationName, inputs, config, lib, pkgs, ... }:
-
+{ config, options, lib, pkgs, ... }:
 let
     cfg = config.moduleLoadout.applications.zen-browser;
 in {
-    options.moduleLoadout.applications.zen-browser = {
-        enable = lib.mkEnableOption "zen-browser module";
-    };
+    options.moduleLoadout.applications.zen-browser.enable = lib.mkEnableOption "zen-browser module";
 
-    config = lib.mkIf cfg.enable {
-        programs.zen-browser  = {
-            enable = true;
-
-            profileVersion = null;
-
-            profiles = {
-                "default" = {
-                    id = 0;
-                    isDefault = true;
-                };
-
-                "wanky" = {
-                    id = 1;
-                    isDefault = false;
+    config = lib.mkIf cfg.enable (
+        {
+            programs.zen-browser = {
+                enable = true;
+                profileVersion = null;
+                profiles = {
+                    default = { id = 0; isDefault = true; };
+                    wanky   = { id = 1; isDefault = false; };
                 };
             };
-        };
-
-        stylix.targets.zen-browser.profileNames = [ "default" ];
-    };
+        } // lib.optionalAttrs (lib.hasAttrByPath [ "stylix" ] options) {
+            stylix.targets."zen-browser".profileNames = [ "default" ];
+        }
+    );
 }
