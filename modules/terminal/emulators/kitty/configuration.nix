7aeea0dff4d6b68cecf3df5ab0795ed73743ab5c
MrCraft18 <mariocaden@gmail.com>
2025-11-05 03:56:40 -0600

    feat: add ssh env for kitty module so terminals dont bug out

--- modules/terminal/emulators/kitty/configuration.nix
+++ modules/terminal/emulators/kitty/configuration.nix
@@ -4,6 +4,11 @@ let
     cfg = config.moduleLoadout.terminal.emulator;
 in {
     config = lib.mkIf (cfg == "kitty") {
+        programs.ssh.extraConfig = ''
+            Host *
+                SetEnv TERM=xterm-256color
+        '';
+
         home-manager.users.craft.moduleLoadout.terminal.emulator = "kitty";
     };
 }
