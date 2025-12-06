89f45cb8c92337796dfbd89f5b91bc94c302c2de
MrCraft18 <mariocaden@gmail.com>
2025-12-05 20:11:31 -0600

    feat: add config so netbook can be used in tablet mode

--- modules/desktops/hyprland-onedarkpro/home.nix
+++ modules/desktops/hyprland-onedarkpro/home.nix
@@ -28,7 +28,10 @@ in {
             yaru-theme
             wl-clipboard
             rofi
-        ];
+        ] ++ (if configurationName == "netbook" then [
+            inputs.iio-hyprland.packages.${pkgs.stdenv.hostPlatform.system}.default
+            wvkbd
+        ] else []);
 
         home.sessionVariables = {
             NIXOS_OZONE_WL = "1";
@@ -46,6 +49,10 @@ in {
 
             package = null;
             portalPackage = null;
+
+            plugins = [
+                inputs.hyprgrass.packages.${pkgs.stdenv.hostPlatform.system}.default
+            ];
 
             settings = {
                 monitor = if configurationName == "netbook" then [
@@ -68,7 +75,10 @@ in {
 
                 exec-once = [
                     "hyprpaper"
-                ];
+                ] ++ (if configurationName == "netbook" then [
+                    "iio-hyprland DSI-1"
+                    "wvkbd-mobintl -L 230 -H 350 --hidden"
+                ] else []);
 
                 "$terminal" = "kitty";
                 "$menu" = "rofi -show drun";            
@@ -284,11 +294,20 @@ in {
                     ", XF86AudioPrev, exec, playerctl previous"
                 ];
 
+                plugin = {
+                    touch_gestures = {
+                        hyprgrass-bind = [
+                            ", edge:d:u, exec, kill -34 $(ps -C wvkbd-mobintl -o pid=)"
+                        ];
+                    };
+                };
+
                 # xwayland.force_zero_scaling = true;
 
                 windowrule = [
                     "match:class .*, suppress_event maximize"
                     "match:class ^$, match:title ^$, match:xwayland true, match:float true, match:fullscreen false, match:pin false, no_focus on"
+                    "match:class ^steam_app_.*, fullscreen_state 2 1, suppress_event fullscreen"
                 ];
             };
        }; 
