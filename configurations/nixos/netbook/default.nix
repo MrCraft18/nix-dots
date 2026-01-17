d3b6e96be5f9ccc770713655685119e9928b71c4
MrCraft18 <mariocaden@gmail.com>
2026-01-17 00:27:16 -0600

    feat(netbook): add copyparty rclone mount

--- configurations/nixos/netbook/default.nix
+++ configurations/nixos/netbook/default.nix
@@ -51,6 +51,51 @@
         obs-studio-plugins.wlrobs
     ];
 
+    home-manager.users.craft.imports = [
+        ({ config, ... }: {
+            sops.secrets."copyparty_craft_password" = {
+                sopsFile = "${inputs.self}/secrets.yaml";
+            };
+
+            sops.templates."rclone-copyparty-headers".content = ''
+                Cookie,cppwd=${config.sops.placeholder."copyparty_craft_password"}
+            '';
+
+            programs.rclone = {
+                enable = true;
+
+                remotes = {
+                    "cpp-rw" = {
+                        config = {
+                            type = "webdav";
+                            vendor = "owncloud";
+                            url = "http://server:3210/";
+                            pacer_min_sleep = "0.01ms";
+                        };
+
+                        secrets.headers = config.sops.templates."rclone-copyparty-headers".path;
+
+                        mounts."/" = {
+                            enable = true;
+                            mountPoint = "${config.home.homeDirectory}/copyparty";
+                            options = {
+                                "vfs-cache-mode" = "full";
+                                "vfs-cache-max-age" = "5s";
+                                "attr-timeout" = "5s";
+                                "dir-cache-time" = "5s";
+                            };
+                        };
+                    };
+                };
+            };
+
+            systemd.user.services."rclone-mount:.@cpp-rw".Unit = {
+                After = [ "rclone-config.service" ];
+                Wants = [ "rclone-config.service" ];
+            };
+        })
+    ];
+
     hardware.sensor.iio.enable = true;
 
     nixpkgs.overlays = [
