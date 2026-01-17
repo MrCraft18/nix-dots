dab76ed3b63449261aa994daea5132263926ae4f
MrCraft18 <mariocaden@gmail.com>
2026-01-17 00:22:32 -0600

    feat(desktop): add copyparty rclone mount

--- configurations/nixos/desktop/default.nix
+++ configurations/nixos/desktop/default.nix
@@ -56,6 +56,51 @@
         inputs.hytale-launcher.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
 
+    home-manager.users.craft.imports = [
+        ({ config, ... }: {
+            sops.secrets."copyparty_craft_password" = {
+                sopsFile = "${inputs.self}/secrets.yaml";
+            };
+
+            sops.templates."rclone-copyparty-headers".content = ''
+                Cookie,cppwd=${config.sops.placeholder."copyparty_craft_password"}
+            '';
+
+            programs.rclone = {
+                enable = true;
+
+                remotes = {
+                    "cpp-rw" = {
+                        config = {
+                            type = "webdav";
+                            vendor = "owncloud";
+                            url = "http://server:3210/";
+                            pacer_min_sleep = "0.01ms";
+                        };
+
+                        secrets.headers = config.sops.templates."rclone-copyparty-headers".path;
+
+                        mounts."/" = {
+                            enable = true;
+                            mountPoint = "${config.home.homeDirectory}/copyparty";
+                            options = {
+                                "vfs-cache-mode" = "full";
+                                "vfs-cache-max-age" = "5s";
+                                "attr-timeout" = "5s";
+                                "dir-cache-time" = "5s";
+                            };
+                        };
+                    };
+                };
+            };
+
+            systemd.user.services."rclone-mount:.@cpp-rw".Unit = {
+                After = [ "rclone-config.service" ];
+                Wants = [ "rclone-config.service" ];
+            };
+        })
+    ];
+
     # Support emulated building for aarch64
     boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
     nix.settings.extra-platforms = config.boot.binfmt.emulatedSystems;
