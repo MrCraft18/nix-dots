9b0afb32d37c01acc2b9c5593a978d7258fe38c8
MrCraft18 <mariocaden@gmail.com>
2026-02-13 17:55:01 -0600

    feat(thinkpad): add copyparty rclone mount

--- configurations/nixos/thinkpad/default.nix
+++ configurations/nixos/thinkpad/default.nix
@@ -46,6 +46,51 @@
         prismlauncher
     ];
 
+    home-manager.users.craft.imports = [
+        ({ config, ... }: {
+            sops.secrets."copyparty_craft_password" = {
+                sopsFile = "${inputs.self}/secrets.yaml";
+            };
+
+            sops.templates."rclone-copyparty-headers".content = ''
+                Cookie,cppwd=${config.sops.placeholder."copyparty_craft_password"}
+            '';
+
+            programs.rclone = {
+                enable = true;
+
+                remotes = {
+                    "cpp-rw" = {
+                        config = {
+                            type = "webdav";
+                            vendor = "owncloud";
+                            url = "http://server:3210/";
+                            pacer_min_sleep = "0.01ms";
+                        };
+
+                        secrets.headers = config.sops.templates."rclone-copyparty-headers".path;
+
+                        mounts."/" = {
+                            enable = true;
+                            mountPoint = "${config.home.homeDirectory}/copyparty";
+                            options = {
+                                "vfs-cache-mode" = "full";
+                                "vfs-cache-max-age" = "5s";
+                                "attr-timeout" = "5s";
+                                "dir-cache-time" = "5s";
+                            };
+                        };
+                    };
+                };
+            };
+
+            systemd.user.services."rclone-mount:.@cpp-rw".Unit = {
+                After = [ "rclone-config.service" ];
+                Wants = [ "rclone-config.service" ];
+            };
+        })
+    ];
+
     system.stateVersion = "25.05";
     home-manager.users.craft.home.stateVersion = "25.05";
 }
