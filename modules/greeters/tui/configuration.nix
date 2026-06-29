0be055f79380c7bb4323b0be5b880fdec6d4a2c1
MrCraft18 <mariocaden@gmail.com>
2026-06-29 00:50:00 -0500

    fix(tui-greeter): fix liteal new lines in greet command

--- modules/greeters/tui/configuration.nix
+++ modules/greeters/tui/configuration.nix
@@ -2,23 +2,29 @@
 
 let
     cfg = config.moduleLoadout.greeter;
+    hyprland = if configurationName == "uconsole" then "uconsole-hyprland" else "hyprland";
+    hyprlandPackage = inputs.${hyprland}.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    startHyprland = "${hyprlandPackage}/bin/start-hyprland";
+    tuigreetCommand = lib.concatStringsSep " " [
+        "${pkgs.tuigreet}/bin/tuigreet"
+        "--greeting 'Welcome Mr_Craft'"
+        "--asterisks"
+        "--remember"
+        "--remember-user-session"
+        "--time"
+        "--cmd ${startHyprland}"
+    ];
 in {
     config = lib.mkIf (cfg == "tui") {
         services.greetd = {
             enable = true;
             settings = {
                 initial_session = {
-                    command = "start-hyprland";
+                    command = startHyprland;
                     user = "craft";
                 };
                 default_session = {
-                    command = "${pkgs.tuigreet}/bin/tuigreet \
-                        --greeting 'Welcome Mr_Craft' \
-                        --asterisks \
-                        --remember \
-                        --remember-user-session \
-                        --time \
-                        --cmd start-hyprland";
+                    command = tuigreetCommand;
                     user = "greeter";
                 };
             };
