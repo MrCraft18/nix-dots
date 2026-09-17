cd92b687d60a691093c9c76b170d157add2d0a41
MrCraft18 <mariocaden@gmail.com>
2026-09-17 21:49:59 +0000

    feat: add more essential pkgs to nix-on-droid

--- modules/essentials/nix-on-droid.nix
+++ modules/essentials/nix-on-droid.nix
@@ -1,6 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+    environment.packages = with pkgs; [
+        coreutils
+        gzip
+        ripgrep
+        dnsutils
+        glibc.bin
+        getent
+        gnused
+        gawk
+        file
+        less
+        procps
+        psmisc
+    ];
+
     home-manager = {
         config = {
             imports = [ ./home.nix ];
